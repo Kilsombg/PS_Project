3f7472323d75e08036e59c4b8fdce071c82d1e97
Kilsom <47924593+Kilsombg@users.noreply.github.com>
2024-02-26 22:11:09 +0200

    include textLogger into master

---
+++ WelcomeExtended/Loggers/TextLogger.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeExtended.Loggers
+{
+    internal class TextLogger : ILogger
+    {
+        private readonly string filePath;
+
+        public TextLogger(string path)
+        {
+            filePath = path;
+        }
+
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            if(!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+            if (formatter != null)
+            {
+                string fullFilePath = Path.Combine(filePath, DateTime.Now.ToString("yyyy-MM-dd") + "_log.txt");
+
+                string exc = "";
+                if (exception != null) exc = " " + exception.GetType() + ": " + exception.Message + " - " + exception.StackTrace + Environment.NewLine;
+                File.AppendAllText(fullFilePath, logLevel.ToString() + ": " + DateTime.Now.ToString() + " " + formatter(state, exception) + exc);
+            }
+        }
+    }
+}
---
+++ WelcomeExtended/Loggers/TextLoggerProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeExtended.Loggers
+{
+    public class TextLoggerProvider : ILoggerProvider
+    {
+        private string path;
+        public TextLoggerProvider(string _path)
+        {
+            path = _path;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new TextLogger(path);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
