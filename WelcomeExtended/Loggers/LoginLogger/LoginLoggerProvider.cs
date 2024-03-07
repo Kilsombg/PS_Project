d3e9d029833549ca0ef714ef5c78c70bb9d4934d
Kilsom <47924593+Kilsombg@users.noreply.github.com>
2024-03-07 21:15:31 +0200

    login logger added

---
+++ WelcomeExtended/Loggers/LoginLogger/LoginLoggerProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeExtended.Loggers.LoginLogger
+{
+    public class LoginLoggerProvider : ILoggerProvider
+    {
+
+        private string path;
+        public LoginLoggerProvider(string _path)
+        {
+            path = _path;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new LoginLogger(path);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
