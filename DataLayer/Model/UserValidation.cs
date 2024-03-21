dbd9a90718a10640bfb040fb84118acd655f67a5
Kilsom <47924593+Kilsombg@users.noreply.github.com>
2024-03-21 23:59:00 +0200

    exercise 4 - ef with SQLite

---
+++ DataLayer/Model/UserValidation.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataLayer.Model
+{
+    public static class UserValidation
+    {
+        public static DatabaseContext context = new DatabaseContext();
+
+        public static bool IsExist(string username, string password)
+        {
+            return context.Users.Any(x => x.Name == username && x.Password == password);
+        }
+    }
+}
